43b9648d72d1fc1d3a87871c7a7a6549b210efd5
PT <trivedp18@gmail.com>
2025-04-10 17:22:57 -0400

    to prompt user for input

--- fetch_messages.py
+++ fetch_messages.py
@@ -4,3 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  
 client = WebClient(token=os.environ['SLACK_API_TOKEN'])
+
+channel = input("Which channel do you want to search? ")
+
+channels = client.conversations_list()['channels']
