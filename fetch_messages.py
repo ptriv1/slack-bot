5c3752d18a7ea639fcb2fdacd86358dc3ffa5cb7
PT <trivedp18@gmail.com>
2025-04-10 18:28:45 -0400

    to print messages and loop through channels

--- fetch_messages.py
+++ fetch_messages.py
@@ -1,10 +1,37 @@
 import os
+from datetime import datetime
 from slack_sdk import WebClient
 from dotenv import load_dotenv
 
 load_dotenv()  
+
 client = WebClient(token=os.environ['SLACK_API_TOKEN'])
 
-channel = input("Which channel do you want to search? ")
+channel_name = input("Which channel do you want to search? ")
 
 channels = client.conversations_list()['channels']
+
+channel_id = None
+
+for channel in channels:
+    if channel['name'] == channel_name:
+        channel_id = channel['id']
+
+if not channel_id:
+    print("Channel not found. Please check the name and try again.")
+    exit()
+
+result = client.conversations_history(channel=channel_id)
+
+print(result)
+
+for message in result['messages']:
+    text = message.get('text', '[no text]')
+    ts = message.get('ts', None)
+
+    if ts:
+        readable_time = datetime.fromtimestamp(float(ts)).strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        readable_time = '[no timestamp]'
+
+    print(f"[{readable_time}] {text}")
